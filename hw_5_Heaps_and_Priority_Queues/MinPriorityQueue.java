c2f3117ed4064b2fd6fdaac69f009413c5c23cc5
danishviking88 <119816815+danishviking88@users.noreply.github.com>
2024-11-13 15:47:40 -0700

    Create MinPriorityQueue.java

---
+++ hw_5_Heaps_and_Priority_Queues/MinPriorityQueue.java
@@ -0,0 +1,49 @@
+
+public class MinPriorityQueue<T extends Comparable<? super T>>
+    implements PriorityQueueInterface<T> {
+
+    private HeapInterface<T> backingHeap;
+    // Do not add any more instance variables. Do not change the declaration
+    // of the instance variables above.
+
+    /**
+     * Creates a priority queue.
+     */
+    public MinPriorityQueue() {
+        backingHeap = new MinHeap<T>();
+    }
+
+    @Override
+    public void enqueue(T item) {
+        backingHeap.add(item);
+    }
+
+    @Override
+    public T dequeue() {
+        return backingHeap.remove();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        // DO NOT MODIFY THIS METHOD!
+        return backingHeap.isEmpty();
+    }
+
+    @Override
+    public int size() {
+        // DO NOT MODIFY THIS METHOD!
+        return backingHeap.size();
+    }
+
+    @Override
+    public void clear() {
+        backingHeap.clear();
+    }
+
+    @Override
+    public HeapInterface<T> getBackingHeap() {
+        // DO NOT MODIFY THIS METHOD!
+        return backingHeap;
+    }
+
+}
